f2afb7a83a47b3a237a8d037fe0d4142ac0c976f
Aryan Jalali <aryanjalali1234@gmail.com>
2024-04-20 01:27:05 -0500

    Added waitress

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 from app import create_app
 
+from pygit2 import Repository
+
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=False)
+    
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
+    # if Repository(".").head.shorthand == "main":
+    #     serve(app, host="0.0.0.0", port=5001)
+    # else:
+    #     app.run(host="0.0.0.0", port=5001, debug=True)
