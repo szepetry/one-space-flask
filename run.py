518fed5b42691e5f6c12dccd3e77845ea47471d7
Aryan Jalali <aryanjalali1234@gmail.com>
2024-04-21 18:55:03 -0500

    Normal app run

--- run.py
+++ run.py
@@ -5,9 +5,10 @@ from pygit2 import Repository
 app = create_app()
     
 if __name__ == "__main__":
-    from waitress import serve
+    # from waitress import serve
     print("Hosting maid service started.")
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
+    # serve(app, host="0.0.0.0", port=5000)
     # if Repository(".").head.shorthand == "main":
     #     serve(app, host="0.0.0.0", port=5001)
     # else:
