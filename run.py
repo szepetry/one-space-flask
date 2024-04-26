da04dc778cc6abb737de13bf86f9ca45d7aa1b90
Aryan Jalali <aryanjalali1234@gmail.com>
2024-04-26 00:12:27 -0500

    Fixed port

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ app = create_app()
 if __name__ == "__main__":
     # from waitress import serve
     print("Hosting maid service started.")
-    # app.run(host="0.0.0.0", port=5001)
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(host="0.0.0.0", port=5001, debug=True)
     # serve(app, host="0.0.0.0", port=5000)
     # if Repository(".").head.shorthand == "main":
     #     serve(app, host="0.0.0.0", port=5001)
