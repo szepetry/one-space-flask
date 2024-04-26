684f22eaab32ab2745d21c29b1fa038d26216fc4
Aryan Jalali <aryanjalali1234@gmail.com>
2024-04-26 00:13:47 -0500

    Added port to .env

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app
-
+import os
 # from pygit2 import Repository
 
 app = create_app()
@@ -7,7 +7,7 @@ app = create_app()
 if __name__ == "__main__":
     # from waitress import serve
     print("Hosting maid service started.")
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=os.getenv("PORT"))
     # app.run(host="0.0.0.0", port=5001, debug=True)
     # serve(app, host="0.0.0.0", port=5000)
     # if Repository(".").head.shorthand == "main":
